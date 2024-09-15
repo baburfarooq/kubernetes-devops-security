3c6bfa1989b144f1b85f3f4ad68c6b725da9eda2
Babur Farooq <babur.farooq@gmail.com>
2024-09-15 18:31:37 +0000

    pdate

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -6,15 +6,9 @@ echo "Docker Image Name: $dockerImageName"
 
 # Run Trivy scan with HIGH severity and capture the exit code
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-
-# Capture the exit code from the Trivy scan
-exit_code=$?
-echo "Exit Code: $exit_code"
-
-# Run Trivy scan with CRITICAL severity
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-# Capture the exit code from the second Trivy scan
+# Capture the exit code from the Trivy scan
 exit_code=$?
 echo "Exit Code: $exit_code"
 
